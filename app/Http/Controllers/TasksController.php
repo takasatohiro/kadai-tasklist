088fdf315619e5194e0192585cf19d033c55f0c5
EC2 Default User <ec2-user@ip-172-31-37-154.ec2.internal>
2019-07-15 13:54:42 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -45,10 +45,12 @@ class TasksController extends Controller
     public function store(Request $request)
     {
         $this->validate($request, [
+            'status' => 'required|max:10',   // 追加
             'content' => 'required|max:191',
         ]);
         
-        $task = new task;
+        $task = new Task;
+        $task->status = $request->status;    // 追加
         $task->content = $request->content;
         $task->save();
 
@@ -95,6 +97,7 @@ class TasksController extends Controller
     public function update(Request $request, $id)
     {
         $this->validate($request, [
+            'status' => 'required|max:10',   // 追加
             'content' => 'required|max:191',
         ]);
 
